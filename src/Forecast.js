1cdde11faaaf5cff34d97f2da73f2d2522f2a337
kerryblue14 <kerryblue14@gmail.com>
2021-04-12 15:56:39 +0100

    forecast added and css
    forecast added
    css added

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,5 +2,45 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
-	return <div className="Forecast">hello from Forecast</div>;
+	return (
+		<div className="Forecast">
+			<div className="row">
+				<div className="col-2 foreDay">
+					<ul>
+						<li>Day</li>
+						<li>img</li>
+						<li>temperature</li>
+					</ul>
+				</div>
+				<div className="col-2 foreDay">
+					<ul>
+						<li>Day</li>
+						<li>img</li>
+						<li>temperature</li>
+					</ul>
+				</div>
+				<div className="col-2 foreDay">
+					<ul>
+						<li>Day</li>
+						<li>img</li>
+						<li>temperature</li>
+					</ul>
+				</div>
+				<div className="col-2 foreDay">
+					<ul>
+						<li>Day</li>
+						<li>img</li>
+						<li>temperature</li>
+					</ul>
+				</div>
+				<div className="col-2 foreDay">
+					<ul>
+						<li>Day</li>
+						<li>img</li>
+						<li>temperature</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
